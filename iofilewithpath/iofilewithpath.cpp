83a667b366572cf40721d83af2b4cac8ae6b3dc7
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-06-04 11:12:50 +0700

    tutup file dan membuka dalam mode membaca file

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -31,6 +31,30 @@ int main()
         //menulis dan memasukkan nilai dari baris  kedalam file
         outfile << baris << endl;
     }
+
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ",txt , ios::in");
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
 
 
