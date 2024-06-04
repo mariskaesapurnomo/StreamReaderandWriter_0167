5e0fb1f9308a442c16b076608840fe7776eee6ec
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-06-04 11:09:50 +0700

    membuka dan menunjuk file

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -10,6 +10,16 @@ int main()
 {
     string baris;
     string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ",txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
 
 
