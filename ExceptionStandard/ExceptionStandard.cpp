95898fd1955d3ff8d3a2bdfe78a5354a3ed113b1
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-06-04 11:17:09 +0700

    Exception Standard

--- ExceptionStandard/ExceptionStandard.cpp
+++ ExceptionStandard/ExceptionStandard.cpp
@@ -21,6 +21,9 @@ int main()
 	catch (exception& e) {
 		//penangkap menggunakan obyek exception
 		cout << e.what() << endl;
-		// akan di eksekusi  karena array data hanya memiliki 3 elemen /
+		// akan di eksekusi  karena array data hanya memiliki 3 elemen 
+		cout << "Baris Program Yang terakhir" << endl;
+		//penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+		return 0;
 	}
 }
