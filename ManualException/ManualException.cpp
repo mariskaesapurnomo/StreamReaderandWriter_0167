a03b7f060068f93e745fd30b56e7867a8d29e55c
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-06-04 11:14:02 +0700

    Manual exception

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
 	try {
@@ -11,13 +12,16 @@ int main()
 		//throw 0.5; //Melemparkan sebuah integer maka
 		cout << "Pernyataan tidak di eksekusi" << endl;
 	}
+
 	catch (int a) {
 		//blok ini akan diekseksui
 		cout << "Pengecualian akan di eksekusi" << endl;
 	}
+
 	catch (...) {
 		// jika selain integer maka  blok ini akan di eksekusi /
 			cout << "default pengecualian dieksekusi" << endl;
 	}
+
 	return 0;
 }
