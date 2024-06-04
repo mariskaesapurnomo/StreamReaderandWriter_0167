6c00e23b708a1bd66aa8e300fbe3da64efd24d2d
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-06-04 10:20:00 +0700

    Add project files.

---
+++ StreamWriterAndReader.cpp
@@ -0,0 +1,12 @@
+// StreamWriterAndReader.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  
+}
